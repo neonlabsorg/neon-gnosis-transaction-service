0500e92e2b55e793182a3af0ca3ceb4b5e14cf30
Daniel Agustin Tradito <datradito@gmail.com>
2021-10-14 16:57:44 -0300

    test: testing coingecko feature

--- safe_transaction_service/tokens/services/price_service.py
+++ safe_transaction_service/tokens/services/price_service.py
@@ -129,6 +129,15 @@ class PriceService:
             except CannotGetPrice:
                 return self.coingecko_client.get_matic_usd_price()
 
+    def get_movr_usd_price(self) -> float:
+        try:
+            return self.coingecko_client.get_movr_usd_price()
+        except CannotGetPrice:
+            try:
+                return self.coingecko_client.get_movr_usd_price()
+            except CannotGetPrice:
+                return self.coingecko_client.get_movr_usd_price()
+
     @cachedmethod(cache=operator.attrgetter('cache_eth_price'))
     @cache_memoize(60 * 30, prefix='balances-get_eth_usd_price')  # 30 minutes
     def get_eth_usd_price(self) -> float:
